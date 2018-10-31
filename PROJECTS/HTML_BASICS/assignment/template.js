5c80dd8e0e0d61b01b8e2d41f8927b2acd4080f0
Arun <sharun@deloitte.com>
2018-10-31 12:15:54 +0530

    first-project

---
+++ PROJECTS/HTML_BASICS/assignment/template.js
@@ -0,0 +1,32 @@
+ //  var a={
+            //             0:{
+            //                 "image":"https://www.w3schools.com/w3images/sandwich.jpg",
+            //                 "head":"The Perfect Sandwich, A Real NYC Classic",
+            //                 "para":"Just some random text, lorem ipsum text praesent tincidunt ipsum lipsum."
+            //             },
+            //             1:{
+            //                 "image":"https://www.w3schools.com/w3images/sandwich.jpg",
+            //                 "head":"The Perfect Sandwich, A Real NYC Classic",
+            //                 "para":"Just some random text, lorem ipsum text praesent tincidunt ipsum lipsum."
+            //             },
+            //             2:{
+            //                 "image":"https://www.w3schools.com/w3images/sandwich.jpg",
+            //                 "head":"The Perfect Sandwich, A Real NYC Classic",
+            //                 "para":"Just some random text, lorem ipsum text praesent tincidunt ipsum lipsum."
+            //             }
+            //  };
+            //  console.log(a[0].image);
+             
+            //      var j="<div class='col-sm-3'><img src="+a[0].image+"alt='Sandwich' style='width:100%'><h3>"+a[0].head+
+            //             "<p>"+a[0].para+"</p></div>";
+            //             var di=document.getElementById('food1');
+            //             di.insertAdjacentHTML('beforeend',j);   
+             
+           
+            function openNav() {
+                document.getElementById("mySidenav").style.width = "500px";
+            }
+            
+            function closeNav() {
+                document.getElementById("mySidenav").style.width = "0";
+            }
